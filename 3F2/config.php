6e8c7efcc7913cc934bfa286062e021591357bc4
lesnansel <kusakalaniosa28@gmail.com>
2024-08-29 12:25:34 +0800

    Add files via upload

---
+++ 3F2/config.php
@@ -0,0 +1,8 @@
+<?php
+define("HOSTNAME", "localhost");
+define("USERNAME","ROOT");
+define("PASSWORD","");
+define("DATABASE","3f2"); 
+
+$connection = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DATABASE);
+?>
